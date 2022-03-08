bffc3a294cb75aa85eb8d2f728b832e0adbff352
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-08 04:22:15 +0000

    a function that returns the tail of an array: everything except for the first item (head) of the provided array, without modifying the array passed in

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`👍🏻 Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+const tail = function(array) {
+  return array.slice(1);
+};
+
+//test code
+assertEqual(tail([]).length, 0);
+console.log(tail([]));
+assertEqual(tail([1]).length, 0);
+console.log(tail([1]));
+assertEqual(tail([0, 1, 2]).length, 2);
+assertEqual(tail([0, 1, 2])[0], 1);
+assertEqual(tail([0, 1, 2])[1], 2);
+console.log(tail([0, 1, 2]));
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
