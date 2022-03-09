8c4c01d4d7ffedbf485f8676d6a649c81be8e69a
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-09 23:35:52 +0000

    a function that when given an array and an object, it will return an object containing counts of everything that the input object listed.

---
+++ countOnly.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`👍🏻 Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item] === undefined) {
+        results[item] = 1;
+      } else {
+        results[item]++;
+      }
+    }    
+  }
+  return results;
+};
+
+//test code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
