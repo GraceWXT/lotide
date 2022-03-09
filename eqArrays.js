401444ae8d43a957e84f06dbcabde082c2bb1c79
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-09 04:45:04 +0000

    Style error corrected

--- eqArrays.js
+++ eqArrays.js
@@ -14,24 +14,24 @@ return result
 */
 const eqArrays = function(arr1, arr2) {
   let result = true;
-  if (arr1.length !== arr2.length){
+  if (arr1.length !== arr2.length) {
     result = false;
   } else {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         result = false;
-      } 
+      }
     }
   }
   return result;
 };
 
 //Test code
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
