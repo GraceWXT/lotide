f4e7f447da04448056a53952a7f29dbc6d73c117
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-14 22:21:21 +0000

    DRYed middle

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  if (arr1.length !== arr2.length) {
-    result = false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-      }
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`👍🏻 Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
-  }
-};
-
 //Requirement: take in an array and return the middle-most element(s) of the given array.
 // For arrays with one or two elements, there is no middle. Return an empty array.
 // For arrays with odd number of elements, an array containing a single middle element should be returned.
@@ -49,19 +26,4 @@ const middle = function(array) {
   return middle;
 };
 
-
-// TEST CODE
-// middle([1]) // => []
-// middle([1, 2]) // => []
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
