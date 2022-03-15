6e4ab594a41845a0d3f5d8eac1bea6c9315b0858
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-15 02:13:25 +0000

    refactored tailTest with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,35 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
+describe("#tail", () => {
+  it("should return empty array when the input array is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("should return empty array when the input array has only one element", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("should returns the 'tail' of an array: everything except for the first item (head) of the provided array", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("should not modify the array that is passed in", () => {
+    const array = [1, 2, 3];
+    tail(array);
+    assert.deepEqual(array, [1, 2, 3]);
+  });
+});
 
-//test code
-assertEqual(tail([]).length, 0);
-console.log(tail([]));
-assertEqual(tail([1]).length, 0);
-console.log(tail([1]));
-assertEqual(tail([0, 1, 2]).length, 2);
-assertEqual(tail([0, 1, 2])[0], 1);
-assertEqual(tail([0, 1, 2])[1], 2);
-console.log(tail([0, 1, 2]));
+// //Original test code
+// assertEqual(tail([]).length, 0);
+// console.log(tail([]));
+// assertEqual(tail([1]).length, 0);
+// console.log(tail([1]));
+// assertEqual(tail([0, 1, 2]).length, 2);
+// assertEqual(tail([0, 1, 2])[0], 1);
+// assertEqual(tail([0, 1, 2])[1], 2);
+// console.log(tail([0, 1, 2]));
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
